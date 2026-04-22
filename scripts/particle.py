b66a03ff416958951aacc855685f32066e8dc41e
d1w0n <yakovlevdillan@outlook.com>
2026-04-22 12:33:43 -0700

    some particle experimentation

--- scripts/particle.py
+++ scripts/particle.py
@@ -8,21 +8,29 @@ class Particle(Instance):
     def __init__(self, window, x, y, width, height, speed, target_x, target_y):
         super().__init__("Particle", "assets/placeholder_red.png", window, x, y, width, height)
         self.speed = speed
+        self._speed_decay = 0.8
         self._dx = target_x - self.x
         self._dy = target_y - self.y
         self._magnitude = self.get_magnitude(self._dx, self._dy)
-        self.x_velocity = self._dx / self._magnitude * self.speed
-        self.y_velocity = self._dy / self._magnitude * self.speed
 
     def update(self, instance_list, room, camera):
+        for instance in instance_list:
+            if instance.type == "Player":
+                if self.get_collision(instance):
+                    self.remove = True
+                    
         self._room_width = room.width
         self._room_height = room.height
         if self.get_room_collision_x() or self.get_room_collision_y():
             self.remove = True
+        
 
     def tick(self):
+        self.x_velocity = self._dx / self._magnitude * self.speed
+        self.y_velocity = self._dy / self._magnitude * self.speed
         self.x += self.x_velocity
         self.y += self.y_velocity
+        self.speed *= self._speed_decay
 
     def render(self, camera_x, camera_y):
         self._window.blit(self._sprite, (self.x - self.width / 2 - camera_x, self.y - self.height / 2 - camera_y))
