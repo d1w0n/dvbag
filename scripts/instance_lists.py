f1efc44ef153af0f12e36f6f409b6ec89499d14e
d1w0n <yakovlevdillan@outlook.com>
2026-05-24 02:38:37 -0700

    more data structure modifications, preparing to allow instance management outside of main loop

---
+++ scripts/instance_lists.py
@@ -0,0 +1,17 @@
+import os
+import pygame
+
+from scripts.player import Player
+from scripts.enemy import Enemy, ProjectileEnemy, ChargerEnemy
+from scripts.projectile import Projectile, Parry, EnemyProjectile, Beam
+from scripts.particle import Particle, EnemyParticle, ProjectileParticle, \
+    ParryFlash, AfterImage, TextDisplay, BeamFade
+import config
+
+pygame.init()
+
+class InstanceLists:
+    def __init__(self, all_instances = []):
+        self.all_instances = all_instances
+        self.add_instances = []
+        self.remove_instances = []
