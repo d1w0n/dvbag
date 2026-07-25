ce8acae56fe5ac669d504854bf1f5f4c96664926
d1w0n <yakovlevdillan@outlook.com>
2026-07-25 15:08:22 -0700

    untrack save.csv instead of all csvs, preparing for using multiple data files for program (like settings, etc.); removed some other file types that are unnecessary to untrack

--- config.py
+++ config.py
@@ -7,4 +7,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MENU_SKIP = False
 IMMORTAL = True
-DEBUG = False
+DEBUG = True
+
+# TODO figure out how to get rid of the config file; use saves directory for settings
