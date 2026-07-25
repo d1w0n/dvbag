8ed69a68b7d99073a87098ec0e7922f29d6ee241
d1w0n <yakovlevdillan@outlook.com>
2026-07-25 15:09:37 -0700

    oops turned on debug

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MENU_SKIP = False
 IMMORTAL = True
-DEBUG = True
+DEBUG = False
 
 # TODO figure out how to get rid of the config file; use saves directory for settings
