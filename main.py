47215eb4df17e4bb568f8a6916a0636088d94b01
d1w0n <yakovlevdillan@outlook.com>
2026-07-27 20:25:57 -0700

    experimenting with smaller rooms

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ tick_pause = 0
 data = {
     "width": width,
     "height": height,
-    "room": Room(width * 2, height * 2),
+    "room": Room(width, height),
     "camera": Camera(window, -width / 2, -height / 2, width, height, 0.1, 0.8),
     "instances": InstanceLists(),
     "ui": UIList(),
