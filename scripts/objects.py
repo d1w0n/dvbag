dee719d30ab8f767f21fe0340a1bbb91eafde389
d1w0n <yakovlevdillan@outlook.com>
2026-07-29 22:51:24 -0700

    created object template

--- scripts/objects.py
+++ scripts/objects.py
@@ -5,5 +5,22 @@ import pygame
 pygame.init()
 
 class Object(ABC):
-    def __init__(self):
+    def __init__(self, type, window, x, y, width, height, color):
+        self.type = type
+        self._window = window
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+        self.remove = False
+
+    def update(self, data):
+        pass
+
+    def tick(self, data):
+        pass
+
+    def render(self, camera):
         pass
